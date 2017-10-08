d0de8a1edec9cb733d093e6e063c775896e5f3c3
gitarzystak <gitarzystak@gmail.com>
2017-10-08 15:35:58 +0200

    html dodany2

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')

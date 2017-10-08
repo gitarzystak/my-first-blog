d0eb1bdbd8cce56ce0d0f06332eb26d87b533bc2
gitarzystak <gitarzystak@gmail.com>
2017-10-08 15:32:50 +0200

    errorpopr

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def post_list(request):
-def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts':posts})
